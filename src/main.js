e3474b6d3b5ae7dd23dd42b2806930fff4a87588
Darya <az@shelest.ua>
2025-10-11 20:54:26 +0300

    update build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import '@fontsource/montserrat/400.css';
-import '@fontsource/montserrat/700.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/700.css';
 import '../assets/fonts/stavok/fonts.css';
 
 import './js/burger';
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
-    base: '/events-shelest/',
+    base: '/',
     root: 'src',
     build: {
       sourcemap: true,
