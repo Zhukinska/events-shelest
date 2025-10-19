9977365504fecd91407bfec31a7b919a061d55bd
Darya <az@shelest.ua>
2025-10-20 00:16:21 +0300

    tg

--- src/js/telegram.js
+++ src/js/telegram.js
@@ -26,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const shortEncoded = encoded.slice(0, 64);
 
       const deepLink = `https://t.me/Event_Shelest_bot?start=${shortEncoded}`;
+      console.log('Telegram link:', deepLink);
 
       window.open(deepLink, '_blank');
     });
